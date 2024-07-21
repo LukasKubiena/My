7dbd5084748c0301fa406f69a935a624c2e3af9d
LukasKubiena <lukasmariakubiena@gmail.com>
2024-07-21 16:41:17 +0200

    test

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,16 @@
 import { Configuration, OpenAIApi } from 'openai';
 
 export default async function handler(req, res) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', 'http://www.lukaskubiena.com');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Handle preflight request
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).end();
   }
