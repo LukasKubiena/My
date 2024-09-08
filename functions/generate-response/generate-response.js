6c3646da672c9963ba4dd8d859becff6dd92fd52
LukasKubiena <lukasmariakubiena@gmail.com>
2024-09-08 09:59:50 +0200

    test

--- functions/generate-response/generate-response.js
+++ functions/generate-response/generate-response.js
@@ -67,7 +67,7 @@ export const handler = async (event) => {
         { role: "system", content: `Resume context: ${resumeContext}` },
         { role: "user", content: userInput }
       ],
-      max_tokens: 200,
+      max_tokens: 150,
       temperature: 0.7,
     });
     console.log('Received response from OpenAI');
